607b0a9be6a8e9e99c7a5d12f63b5ac885b4fbdf
jdcowe <jordancowe@gmail.com>
2019-09-05 06:43:19 -0400

    Basic API and Login

---
+++ server.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", '$2b$10$hcNxTE8b.HYYzHs2eWlS1ux/whjgHbnI2tmoIKaeNL8D9DgtgDZ62', function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", '$2b$10$hcNxTE8b.HYYzHs2eWlS1ux/whjgHbnI2tmoIKaeNL8D9DgtgDZ62', function(err, res) {
+        console.log('second guess', res);
+    });
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success')
+    } else {
+        res.status(400).json('error login failed');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        console.log(hash);
+    });
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    )
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req,res) => {
+    const {id} = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            return res.json(user);
+        }
+    })
+
+    if (found === false) {
+        return res.status(400).json('not found');
+    }
+})
+
+app.post('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (found === false) {
+        return res.status(400).json('not found');
+    }
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+
+
+
+// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
+//     // res == false
+// });
+
+/*  
+import bcrypt from'bcrypt'
+const saltRounds = 10 // increase this if you want more iterations  
+const userPassword = 'supersecretpassword'  
+const randomPassword = 'fakepassword'
+ 
+const storeUserPassword = (password, salt) =>  
+  bcrypt.hash(password, salt).then(storeHashInDatabase)
+ 
+const storeHashInDatabase = (hash) => {  
+   // Store the hash in your password DB
+   return hash // For now we are returning the hash for testing at the bottom
+}
+ 
+// Returns true if user password is correct, returns false otherwise
+const checkUserPassword = (enteredPassword, storedPasswordHash) =>  
+  bcrypt.compare(enteredPassword, storedPasswordHash)
+ 
+ 
+// This is for demonstration purposes only.
+storeUserPassword(userPassword, saltRounds)  
+  .then(hash =>
+    // change param userPassword to randomPassword to get false
+    checkUserPassword(userPassword, hash)
+  )
+  .then(console.log)
+  .catch(console.error) */
