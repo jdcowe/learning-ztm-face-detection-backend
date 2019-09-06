91e02221c727b74b2259d765d9dbe9127f114188
Jordan Cowe <jordancowe@gmail.com>
2019-09-06 15:35:55 -0400

    Connected to Database, Added Face Detection API

--- server.js
+++ server.js
@@ -1,133 +1,128 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-const cors = require('cors');
+const clarifai = require("clarifai");
+const cors = require("cors");
+const knex = require("knex");
+const config = require("./config.js");
+
+const clarifaiApp = new Clarifai.App({
+  apiKey: config.apiKey
+});
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "",
+    password: "",
+    database: "smart-brain"
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+app.post("/imageUrl", (req, res) => {
+  clarifaiApp.models
+    .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("Api error"));
+});
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
 
-app.post('/signin', (req, res) => {
-    
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0])
-    } else {
-        res.status(400).json('error login failed');
-    }
-})
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Invalid");
+  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentials"));
+});
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        console.log(hash);
-    });
-    database.users.push(
-        {
-            id: '125',
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("Invalid");
+  }
+  const hash = bcrypt.hashSync(password, saltRounds);
+
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
             name: name,
-            email: email,
-            password: password,
-            entries: 0,
             joined: new Date()
-        }
-    )
-    res.json(database.users[database.users.length-1]);
-})
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      });
+  }).catch(err => res.status(400).json("Unable to Reigster"));
+});
 
-app.get('/profile/:id', (req,res) => {
-    const {id} = req.params;
-    let found = false;
-
-    database.users.forEach(user => {
-        if (user.id === id) {
-            return res.json(user);
-        }
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("not found");
+      }
     })
+    .catch(err => res.status(400).json("Error Getting User"));
+});
 
-    if (found === false) {
-        return res.status(400).json('not found');
-    }
-})
-
-app.put('/image', (req, res) => {
-    const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
     })
-    if (found === false) {
-        return res.status(400).json('not found');
-    }
-})
+    .catch(err => res.status(400).json("unable to get entries"));
+});
 
 app.listen(3000, () => {
-    console.log('app is running on port 3000');
-})
-
-
-
-
-// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
-//     // res == false
-// });
-
-/*  
-import bcrypt from'bcrypt'
-const saltRounds = 10 // increase this if you want more iterations  
-const userPassword = 'supersecretpassword'  
-const randomPassword = 'fakepassword'
- 
-const storeUserPassword = (password, salt) =>  
-  bcrypt.hash(password, salt).then(storeHashInDatabase)
- 
-const storeHashInDatabase = (hash) => {  
-   // Store the hash in your password DB
-   return hash // For now we are returning the hash for testing at the bottom
-}
- 
-// Returns true if user password is correct, returns false otherwise
-const checkUserPassword = (enteredPassword, storedPasswordHash) =>  
-  bcrypt.compare(enteredPassword, storedPasswordHash)
- 
- 
-// This is for demonstration purposes only.
-storeUserPassword(userPassword, saltRounds)  
-  .then(hash =>
-    // change param userPassword to randomPassword to get false
-    checkUserPassword(userPassword, hash)
-  )
-  .then(console.log)
-  .catch(console.error) */
+  console.log("app is running on port 3000");
+});
